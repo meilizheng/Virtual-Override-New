608f0b04954d51c618379506e2f9a1aa70153f39
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-05-05 23:50:19 -0700

    add comment
    add comment

--- CheckingAccount.cs
+++ CheckingAccount.cs
@@ -9,22 +9,22 @@ namespace Virtual_Override_New
 {
     public class CheckingAccount : BankAccount //created an inheritance class;
     {
-        double _OverdraftFee;
+        double _OverdraftFee; //created a field;
 
-        public CheckingAccount(string name, double balance, double overdraftFee = 50) : base(name, balance)
+        public CheckingAccount(string name, double balance, double overdraftFee = 50) : base(name, balance)//created constructor based on parent class(bank account)
         {
             _OverdraftFee = overdraftFee;
         }
 
-        public override void Deposit(double amount)
+        public override void Deposit(double amount) //created a deposit method based on parent class;
         {
-            if (amount > 0)
+            if (amount > 0)//if amount greater than zero, add it to balance;
             {
                 Balance += amount;
             }
             else
             {
-                Console.WriteLine("please check you deposit amount.");
+                Console.WriteLine("please check you deposit amount.");//else notify user for the invalid input;
             }
         }
 
